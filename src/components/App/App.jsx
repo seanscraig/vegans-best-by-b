c157a3086c6d293c89b5df4e8f854968c1a4ff76
Sean Craig <seancraig21@gmail.com>
2023-02-19 10:00:03 -0600

    Got the background image to scale properly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,8 +8,10 @@ import Footer from "../Footer";
 const containerStyle = {
   flex: "1 0 auto",
   backgroundImage: `url(${backgroundImage})`,
+  backgroundPosition: "center center",
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
+  backgroundColor: "#464646"
 };
 
 function App() {
