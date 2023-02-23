6ff4498061cc5bbf7d9223d5cf2cfa317273dbb9
Sean Craig <seancraig21@gmail.com>
2023-02-23 16:00:35 -0600

    Got nav working and added hamburger menu for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
@@ -25,14 +26,23 @@ const navItems = [
 ];
 
 const NavBar = () => {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+
   return (
     <nav className="nav-bar">
-      <div className="logo-container">
-        <NavLink to="/" end>
-          <span className="logo">Vegans Best By B</span>
-        </NavLink>
+      <div className="nav-logo-and-button">
+        <div className="logo-container">
+          <NavLink to="/" end>
+            <span className="logo">Vegans Best By B</span>
+          </NavLink>
+        </div>
+        <div className="hamburger-menu-button">
+          <button onClick={() => setNavMenuOpen((prev) => !prev)}>
+            {navMenuOpen ? "close" : "open"}
+          </button>
+        </div>
       </div>
-      <ul>
+      <ul className={`nav-links${navMenuOpen ? " show-links" : ""}`}>
         {navItems.map((item) => (
           <li key={item.id}>
             <NavLink
