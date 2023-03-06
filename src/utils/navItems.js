5a52c6f5d8c211dbddc6d975f814d5554020d0c7
Sean Craig <seancraig21@gmail.com>
2023-03-06 16:13:10 -0600

    refactored navItems and productItems

---
+++ src/utils/navItems.js
@@ -0,0 +1,27 @@
+export const navItems = [
+  {
+    id: 0,
+    text: "Home",
+    path: "/",
+  },
+  {
+    id: 1,
+    text: "About Us",
+    path: "about",
+  },
+  {
+    id: 2,
+    text: "Products",
+    path: "products",
+  },
+  {
+    id: 3,
+    text: "Shop",
+    path: "shop",
+  },
+  {
+    id: 4,
+    text: "Contact",
+    path: "contact",
+  },
+];
---
+++ src/utils/productItems.js
@@ -0,0 +1,51 @@
+import productImg0 from "../assets/images/products/productImg0.jpg";
+import productImg1 from "../assets/images/products/FB_IMG_1659994072491.jpg";
+import productImg2 from "../assets/images/products/IMG_20230126_174342.jpg";
+import productImg3 from "../assets/images/products/IMG_20230128_122529.jpg";
+import productImg4 from "../assets/images/products/IMG_20221028_112116__01.jpg";
+import productImg5 from "../assets/images/products/IMG_20230210_123046.jpg";
+
+export const productItems = [
+  {
+    id: 0,
+    img: productImg0,
+    name: "Avocado Chocolate Mousse",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+  {
+    id: 1,
+    img: productImg1,
+    name: "Tacos",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+  {
+    id: 2,
+    img: productImg2,
+    name: "Name",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+  {
+    id: 3,
+    img: productImg3,
+    name: "Name",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+  {
+    id: 4,
+    img: productImg4,
+    name: "Name",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+  {
+    id: 5,
+    img: productImg5,
+    name: "Name",
+    description:
+      "This is where we would put a description of the product/recipe",
+  },
+];
