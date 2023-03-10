cf12a2674644b68ceaefcd316ab395f4c1b6e20b
Sean Craig <seancraig21@gmail.com>
2023-03-10 16:04:43 -0600

    Added icon to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
+import { IconContext } from "react-icons";
+import { FiInstagram } from "react-icons/fi";
 import "./Footer.css";
 
 const Footer = function () {
   return (
     <div className="footer">
-      <span>Socials</span>
+      <a href="http://www.google.com">
+        Follow us on <FiInstagram />
+      </a>
       <span>
         Designed and Developed by <a href="http://seancraig.me">Sean Craig</a>
       </span>
