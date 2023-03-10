d7fa11bf289c9a0b0f6370a6f4584db3a66deaef
Sean Craig <seancraig21@gmail.com>
2023-03-10 16:16:29 -0600

    renamed images and added a name to a product

--- src/utils/productItems.js
+++ src/utils/productItems.js
@@ -1,9 +1,9 @@
-import productImg0 from "../assets/images/products/productImg0.jpg";
-import productImg1 from "../assets/images/products/FB_IMG_1659994072491.jpg";
-import productImg2 from "../assets/images/products/IMG_20230126_174342.jpg";
-import productImg3 from "../assets/images/products/IMG_20230128_122529.jpg";
-import productImg4 from "../assets/images/products/IMG_20221028_112116__01.jpg";
-import productImg5 from "../assets/images/products/IMG_20230210_123046.jpg";
+import productImg0 from "../assets/images/products/productImg1.jpg";
+import productImg1 from "../assets/images/products/productImg2.jpg";
+import productImg2 from "../assets/images/products/productImg3.jpg";
+import productImg3 from "../assets/images/products/productImg4.jpg";
+import productImg4 from "../assets/images/products/productImg5.jpg";
+import productImg5 from "../assets/images/products/productImg6.jpg";
 
 export const productItems = [
   {
@@ -23,7 +23,7 @@ export const productItems = [
   {
     id: 2,
     img: productImg2,
-    name: "Name",
+    name: "Coconut Alfredo Curry Pasta",
     description:
       "This is where we would put a description of the product/recipe",
   },
